d22cc28e55f5155eb82fd3e193adbfea66073f49
mustafarh <ibraheemmustafah3@gmail.com>
2022-12-16 15:58:54 -0800

    isupper

---
+++ 0x03-debugging/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main_h"
+/**
+ * _isupper -function that check for uppercase character.
+ * @c: input
+ * Return: i if c is uppercase, O otherwise
+ */
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+
+return (1);
+
+else
+return (0);
+}
